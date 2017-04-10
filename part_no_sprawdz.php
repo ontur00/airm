dcf8724ee449251358fd7197bb6a4dde36e726b1
agarbacikseoyon <agarbacik.seoyon@gmail.com>
2017-04-10 11:09:21 +0200

    bug fixed dla ALC ze znakiem  -

--- part_no_sprawdz.php
+++ part_no_sprawdz.php
@@ -7,11 +7,13 @@
 <META http-equiv="Content-Type" content="text/html; charset=ISO-8859-2">
 
 <?php
+//ini_set( 'display_errors', 'On' ); 
+//error_reporting( E_ALL );
 function ZwrocKlienta($nr_zamowienia, $db, $db_login, $db_haslo)
 {
 	mysql_connect('localhost', $db_login, $db_haslo);
 	mysql_query('SET CHARSET latin2');
-	@mysql_select_db($db) or die("Nie można znaleć bazy danych!");
+	@mysql_select_db($db) or die("Nie moďż˝na znaleďż˝ďż˝ bazy danych!");
 	$kwerenda = "SELECT * FROM zamowienia WHERE nr_zam='" . $nr_zamowienia . "' ";
 	$wynik = mysql_query($kwerenda);
 	$klient = mysql_result($wynik, 0, "klient");
@@ -24,7 +26,7 @@ function ZwrocAlc($part_no, $db, $db_login, $db_haslo)
 {
 	mysql_connect('localhost', $db_login, $db_haslo);
 	mysql_query('SET CHARSET latin2');
-	@mysql_select_db($db) or die("Nie można znaleć bazy danych!");
+	@mysql_select_db($db) or die("Nie moďż˝na znaleďż˝ďż˝ bazy danych!");
 	$kwerenda = "SELECT * FROM prod_got WHERE shinchang_part_no='" . $part_no . "' ";
 	$wynik = mysql_query($kwerenda);
 	$alc_z_bazy = mysql_result($wynik, 0, "nr_etyk");
@@ -39,11 +41,18 @@ function UsunSpacje($tekst)
 	return $zwrot;
 }
 
+function WyszukajMyslnik($kodALC)
+{
+	 $terminator = strpos($kodALC, "-"); 
+	 echo "WyszukajMyslnik=".$terminator;
+	 return $terminator;
+}
+
 function ZapiszLog($part_no, $stan, $login, $powod, $nr_etyk, $barcodes, $nr_zam, $klient, $db, $db_login, $db_haslo)
 {
 	mysql_connect('localhost', $db_login, $db_haslo);
 	mysql_query('SET CHARSET latin2');
-	@mysql_select_db($db) or die("Nie można znaleć bazy danych!");
+	@mysql_select_db($db) or die("Nie moďż˝na znaleďż˝ďż˝ bazy danych!");
 	$kwerenda = "
 	INSERT INTO prod_par_wys_alc (part_no, stan, login, powod, nr_etyk, barcodes, nr_zam, klient)
 	VALUES ('" . $part_no . "', '" . $stan . "', '" . $login . "', '" . $powod . "',
@@ -97,9 +106,10 @@ $komunikat = "<center><B><FONT COLOR='#00aa00'>Klient : " . $nazwa_klienta . " <
 echo $komunikat;
 */
 
-
+//echo "<br></br>TEM TUTAJ1";
 if($dluglogin<2)
 { 
+	//echo "<br></br>TEM TUTAJ2";
 	echo "<font size='0'>"; 
 	echo include "wyloguj.php"; 
 	$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>Zaloguj ponownie<BR>. 
@@ -108,7 +118,7 @@ if($dluglogin<2)
 }
 else 
 {																	
-																
+	//echo "<br></br>TEM TUTAJ3";																
 	$dlug = strlen($kod);
 	$data_rok = substr($kod, 0,4);
 	$data_mies = substr($kod, 4,2);
@@ -130,9 +140,10 @@ else
       
     if($dlug<27)
 	{
+		//echo "<br></br>TEM TUTAJ4";
 		$qty_box		   = substr($kod ,10,4);
 	}
-	
+	//echo "<br></br>TEM TUTAJ5";
 	settype($qty_box, "integer");
 
 	
@@ -143,7 +154,7 @@ else
 
 	mysql_connect('localhost',$uzytkownik,$haslo);
 	mysql_query('SET CHARSET latin2');
-	@mysql_select_db($baza) or die("Nie można znaleć bazy danych!");
+	@mysql_select_db($baza) or die("Nie moďż˝na znaleďż˝ďż˝ bazy danych!");
 	$kwerenda5reglkjt = "SELECT * FROM prod_got WHERE shinchang_part_no='$shinchang_part_no'";
 	$wynik5reglkjt = mysql_query($kwerenda5reglkjt);
 	$rekordow5reglkjt = mysql_numrows($wynik5reglkjt);
@@ -154,7 +165,7 @@ else
 
 	mysql_connect('localhost',$uzytkownik,$haslo);
 	mysql_query('SET CHARSET latin2');
-	@mysql_select_db($baza) or die("Nie można znaleć bazy danych!");
+	@mysql_select_db($baza) or die("Nie moďż˝na znaleďż˝ďż˝ bazy danych!");
 	$kwerenda5reglkjtd = "SELECT * FROM prod_got_wys WHERE barcode='$kod' and pojazd='$kodzam'";
 	$wynik5reglkjtd = mysql_query($kwerenda5reglkjtd);
 	$rekordow5reglkjtd = mysql_numrows($wynik5reglkjtd);
@@ -165,7 +176,7 @@ else
 
 	mysql_connect('localhost',$uzytkownik,$haslo);
 	mysql_query('SET CHARSET latin2');
-	@mysql_select_db($baza) or die("Nie można znaleć bazy danych!");
+	@mysql_select_db($baza) or die("Nie moďż˝na znaleďż˝ďż˝ bazy danych!");
 	$kwerenda5reglkjtds = "SELECT * FROM zamowienia WHERE  nr_zam='$kodzam'";
 	$wynik5reglkjtds = mysql_query($kwerenda5reglkjtds);
 	$rekordow5reglkjtds = mysql_numrows($wynik5reglkjtds);
@@ -176,7 +187,7 @@ else
 										
 	mysql_connect('localhost',$uzytkownik,$haslo);
 	mysql_query('SET CHARSET latin2');
-	@mysql_select_db($baza) or die("Nie można znaleć bazy danych!");
+	@mysql_select_db($baza) or die("Nie moďż˝na znaleďż˝ďż˝ bazy danych!");
 	$kwerenda5reglkj = "SELECT * FROM prod_st WHERE barcode='$kod'";
 	$wynik5reglkj = mysql_query($kwerenda5reglkj);
 	$rekordow5reglkj = mysql_numrows($wynik5reglkj);
@@ -211,7 +222,7 @@ else
 																
 	mysql_connect('localhost',$uzytkownik,$haslo);
 	mysql_query('SET CHARSET latin2');
-	@mysql_select_db($baza) or die("Nie można znaleć bazy danych!");
+	@mysql_select_db($baza) or die("Nie moďż˝na znaleďż˝ďż˝ bazy danych!");
 	$kwerenda5 = "SELECT * FROM zamowienia_zaw WHERE nr_zam='$kodzam' and shinchang_part_no='$shinchang_part_no'";
 	$wynik5 = mysql_query($kwerenda5);
 	$rekordow5 = mysql_numrows($wynik5);
@@ -227,7 +238,7 @@ else
 
 	mysql_connect('localhost',$uzytkownik,$haslo);
 	mysql_query('SET CHARSET latin2');
-	@mysql_select_db($baza) or die("Nie można znaleć bazy danych!");
+	@mysql_select_db($baza) or die("Nie moďż˝na znaleďż˝ďż˝ bazy danych!");
 	$kwerendalv = "SELECT * FROM prod_kody WHERE code='$kodzam'";
 	$wyniklv = mysql_query($kwerendalv);
 	$rekordowlv = mysql_numrows($wyniklv);
@@ -236,13 +247,16 @@ else
 	$nr_opis	 			= mysql_result($wyniklv, 0, "opis");
 	$dl_nr_code= strlen($nr_code); 
  
+ 	//echo "<br></br>TEM TUTAJ6";
 	if($dl_nr_code>1)
 	{
+		//echo "<br></br>TEM TUTAJ7";
 		$qty_przyg=$qty_box;
 		$qty_zam=$qty_box+$qty_box;
 		$qty_box_pr=$qty_box;
 	}
 	
+	//echo "<br></br>TEM TUTAJ8";
 	session_start();
 	$_SESSION['sbarcodew']=$kod;
 	$dlug_part_no = strlen($part_no);
@@ -254,6 +268,7 @@ else
 	
 	if(($nr_wys_br===$kodzam)and($lok_br==='MW'))
 	{ 
+		//echo "<br></br>TEM TUTAJ9";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>BOX JEST JUZ ZAREJESTOWANY DO OBECNEJ WYSYLKI NR $kodzam<BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -262,6 +277,7 @@ else
 	}	
 	elseif(($nr_zam!==$kodzam)and($dl_nr_code<1)and($dlug>26))
 	{ 
+		//echo "<br></br>TEM TUTAJ10";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>NIEPRAWIDLOWY BOX DO OBECNEJ WYSYLKI NR $kodzam $nr_zam<BR></FONT><FONT COLOR='#0000FF'> ZESKANUJ PRAWIDLOWY BOX TEN JEST DO INNEJ WYSYLKI. 
@@ -270,6 +286,7 @@ else
 	}	
 	elseif($dlug_scpr_pr>1)
 	{ 
+		//echo "<br></br>TEM TUTAJ11";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>BOX JEST JUZ ZAREJESTOWANY DO OBECNEJ WYSYLKI NR $kodzam<BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -278,6 +295,7 @@ else
 	}	
 	elseif($qty_box<1)
 	{ 
+		//echo "<br></br>TEM TUTAJ12";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>BLAD ODCZYTU BARCODE w ILOSCI <BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -286,6 +304,7 @@ else
 	}	
 	elseif($lok_br==='ZB')
 	{ 
+		//echo "<br></br>TEM TUTAJ13";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>BOX JEST JUZ ZABLOKOWANY, W STREFIE PRODUKTOW ZABLOKOWANYCH NR $nr_wys_br<BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -294,6 +313,7 @@ else
 	}	
 	elseif($lok_br==='MW')
 	{ 
+		//echo "<br></br>TEM TUTAJ14";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>BOX JEST JUZ ZAREJESTOWANY DO INNEJ WYSYLKI NR $nr_wys_br<BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -302,6 +322,7 @@ else
 	}	
 	elseif($lok_br==='WZ')
 	{ 
+		//echo "<br></br>TEM TUTAJ15";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>BOX JEST NA WYSYLKA ZAMKNIETA NR $nr_wys_br<BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -310,6 +331,7 @@ else
 	}	
 	elseif($lok_br==='MP')
 	{ 
+		//echo "<br></br>TEM TUTAJ16";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>BOX JEST NA PRZYJECIU Z PRODUKCJI<BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -318,6 +340,7 @@ else
 	}	
 	elseif($lok_br==='PR')
 	{ 
+		//echo "<br></br>TEM TUTAJ17";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>BOX JEST WYDANY NA REWORK<BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -326,6 +349,7 @@ else
 	}
 	elseif($lok_br==='')
 	{ 
+		//echo "<br></br>TEM TUTAJ18";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>BOX nie jest zarejestrowany w systemie lub bledny kod kreskowy<BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -334,6 +358,7 @@ else
 	} 
 	elseif(($qty_przygr>$qty_zammin) and ($dlug_part_no>1))
 	{ 
+		//echo "<br></br>TEM TUTAJ19";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>ZA DUZA ILOSC $PART_NO BOX DO WYSYLKI NR $kodzam<BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -342,6 +367,7 @@ else
 	}	 				 
 	elseif(($qty_box>$qty_box_pr1) and ($dlug_part_no>1))
 	{ 
+		//echo "<br></br>TEM TUTAJ20";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>ZA DUZA ILOSC NA ETYKIECIE $PART_NO BOX <BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -350,6 +376,7 @@ else
 	}
 	elseif(($dlugpart_name_br<1) and ($dlug>26))
 	{ 
+		//echo "<br></br>TEM TUTAJ21";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>BLAD ETYKIETY ZESKANUJ JESZCZE RAZ <BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -358,6 +385,7 @@ else
 	}	
 	elseif($dlug_scpr_pr>1)
 	{ 
+		//echo "<br></br>TEM TUTAJ22";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>WYSTEPUJE W WYSYLCE ETYKIETY ZESKANUJ JESZCZE RAZ <BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -366,9 +394,11 @@ else
 	}	
 	elseif(($dl_nr_code<1) and($dlug>26)and ($dlug_klientwm1>2))
 	{  
+		//echo "<br></br>TEM TUTAJ23";
 						 
 		if($P_lot_no!==$lot_no_baza)
 		{ 
+			//echo "<br></br>TEM TUTAJ24";
 			echo "<font size='0'>"; 
 			echo include "podaj_part_no_wys.php"; 
 			$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>LOT_NO NIEZGODNY Z FIFO LISTY WYSYLKI<BR></FONT><FONT COLOR='#0000FF'> WSZYTAJ BOX Z PRAWIDLOWYM LOT NO !! 
@@ -377,12 +407,14 @@ else
 		}
 		else
 		{
+			//echo "<br></br>TEM TUTAJ25";
 			$alc_baza = ZwrocAlc( substr($kod , 8, 11) , $baza, $uzytkownik, $haslo);
 			$alc_baza = UsunSpacje($alc_baza);
-			
+			//echo "<br></br>TEM alc_baza= $alc_baza  formalc= $formalc";
 			
 			if ( ( !isset($formalc) || strlen($formalc) == 0 ) && strlen($alc_baza) > 0 && $alc_baza !== "0" )
 			{
+				//echo "<br></br>TEM TUTAJ26";
 				echo include "produkt_gotowy_alc_TEST.php"; 
 				$komunikat = "<center><B><FONT COLOR='#00aa00'>WCZYTAJ KOD ALC <BR>  
 				</center><hr>";
@@ -390,15 +422,20 @@ else
 			}
 			else
 			{
+				//echo "<br></br>TEM TUTAJ27";
 				if( strlen($formalc) > 20 )
 				{
-				
+					//echo "<br></br>TEM TUTAJ28";	
 					$formalc = substr( $formalc , 0, 12 );
 				}
 				elseif( strlen($formalc) > strlen($alc_baza) )
 				{
-					$dlugosc_uciecie = strlen($formalc) - strlen($alc_baza);
-					$formalc = substr( $formalc , $dlugosc_uciecie, strlen($alc_baza) );
+					//echo "<br></br>TEM TUTAJ29";
+					if(WyszukajMyslnik($formalc)!=0)
+					//$dlugosc_uciecie = strlen($formalc) - strlen($alc_baza);
+					$dlugosc_uciecie = WyszukajMyslnik($formalc);
+					$formalc = substr( $formalc , 0, $dlugosc_uciecie );
+					//echo "<br></br> dlugoscUciecia=$dlugosc_uciecie alc=".$formalc." alc_baza=".$alc_baza;
 				}
 				
 				
@@ -422,9 +459,10 @@ else
 				<br>
 				";
 				*/
-				
+				//echo "<br></br>TEM TUTAJ30";
 				if( strlen($alc_baza) == 0 || $alc_baza == "0" )
 				{
+					//echo "<br></br>TEM TUTAJ31";
 					//VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
 					
 					require "incl/wys_zapis_a.php";
@@ -436,7 +474,7 @@ else
 				}
 				elseif($alc_baza == $formalc)
 				{
-					
+					//echo "<br></br>TEM TUTAJ32";
 					if ( !isset($licznik) || $licznik == NULL || $licznik <= 0 )
 					{
 						$licznik = 1;
@@ -444,6 +482,7 @@ else
 					
 					if ( $licznik <= ($qty_box - 1) && ( $nazwa_klienta == "KMS" || $nazwa_klienta == "HMMC" || $nazwa_klienta == "KOMOS" ) )
 					{
+						//echo "<br></br>TEM TUTAJ33";
 						$licznik++;
 						echo include "produkt_gotowy_alc_TEST.php"; //inkrementowany $licznik przekazywany do formularza!
 						$komunikat = "<center><B><FONT COLOR='#00aa00'>KOD ALC ( " . ( $licznik - 1 ) . " z " . $qty_box . " ) OK<BR>WCZYTAJ NASTEPNY KOD!<BR>					
@@ -453,6 +492,7 @@ else
 					}
 					else
 					{
+						//echo "<br></br>TEM TUTAJ34";
 						//VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
 						
 						require "incl/wys_zapis_a.php";
@@ -462,17 +502,18 @@ else
 						
 						//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 					}
-					
+					//echo "<br></br>TEM TUTAJ35";
 				}
 				else
 				{
+					//echo "<br></br>TEM TUTAJ36";
 					echo include "produkt_gotowy_alc_TEST.php"; 
 					$komunikat = "<center><B><FONT COLOR='#FF0000'>KOD ALC NIEZGODNY !<BR>WCZYTAJ KOD ALC<BR>  
 					</center><hr>";
 					echo $komunikat;
 				}
 				
-				
+				//echo "<br></br>TEM TUTAJ37";
 			}
 			
 			
@@ -490,10 +531,12 @@ else
 	}
 	elseif($dlug_qty_box_pr<1)
 	{ 
+		//echo "<br></br>TEM TUTAJ38";
 		echo include "parowanie_klient_kod.php"; 
 	}	
 	elseif(($dlug_part_no<1)and($dl_nr_code<1))
 	{ 
+		//echo "<br></br>TEM TUTAJ39";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>NIEPRAWIDLOWY BOX DO WYSYLKI NR $kodzam<BR></FONT><FONT COLOR='#0000FF'> ZAREJESTRUJ OPERACJE JESZCZE RAZ. 
@@ -502,11 +545,13 @@ else
 	}	
 	elseif(($lok_br==='MG')and($dl_nr_code<1)) 
 	{  
+		//echo "<br></br>TEM TUTAJ40";
 		$alc_baza = ZwrocAlc( substr($kod , 8, 11) , $baza, $uzytkownik, $haslo);
 		$alc_baza = UsunSpacje($alc_baza);
 		
 		if ( ( !isset($formalc) || strlen($formalc) == 0 ) && strlen($alc_baza) > 0 && $alc_baza !== "0" )
 		{
+			//echo "<br></br>TEM TUTAJ41";
 			echo include "produkt_gotowy_alc_TEST.php"; 
 			$komunikat = "<center><B><FONT COLOR='#00aa00'>WCZYTAJ KOD ALC <BR>  
 			</center><hr>";
@@ -514,6 +559,7 @@ else
 		}
 		else
 		{
+			//echo "<br></br>TEM TUTAJ42";
 			if( strlen($formalc) > strlen($alc_baza) )
 			{
 				$dlugosc_uciecie = strlen($formalc) - strlen($alc_baza);
@@ -542,9 +588,10 @@ else
 			<br>
 			";
 			*/
-			
+			//echo "<br></br>TEM TUTAJ43";
 			if( strlen($alc_baza) == 0 || $alc_baza == "0" )
 			{
+				//echo "<br></br>TEM TUTAJ44";
 				//VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
 				
 				require "incl/wys_zapis_a.php";
@@ -556,6 +603,7 @@ else
 			}
 			elseif($alc_baza == $formalc)
 			{
+				//echo "<br></br>TEM TUTAJ45";
 				if ( !isset($licznik) || $licznik == NULL || $licznik <= 0 )
 				{
 					$licznik = 1;
@@ -563,6 +611,7 @@ else
 				
 				if ( $licznik <= ($qty_box - 1) && ( $nazwa_klienta == "KMS" || $nazwa_klienta == "HMMC" || $nazwa_klienta == "KOMOS" ) )
 				{
+					//echo "<br></br>TEM TUTAJ46";
 					$licznik++;
 					echo include "produkt_gotowy_alc_TEST.php"; //inkrementowany $licznik przekazywany do formularza!
 					$komunikat = "<center><B><FONT COLOR='#00aa00'>KOD ALC ( " . ( $licznik - 1 ) . " z " . $qty_box . " ) OK<BR>WCZYTAJ NASTEPNY KOD!<BR>					
@@ -572,6 +621,7 @@ else
 				}
 				else
 				{
+					//echo "<br></br>TEM TUTAJ47";
 					//VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
 					
 					require "incl/wys_zapis_a.php";
@@ -581,10 +631,11 @@ else
 					
 					//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 				}
-				
+				//echo "<br></br>TEM TUTAJ48";
 			}
 			else
 			{
+				//echo "<br></br>TEM TUTAJ49";
 				echo include "produkt_gotowy_alc_TEST.php"; 
 				$komunikat = "<center><B><FONT COLOR='#FF0000'>KOD ALC NIEZGODNY !<BR>WCZYTAJ KOD ALC<BR>  
 				</center><hr>";
@@ -596,11 +647,13 @@ else
 	}
 	elseif(($lok_br==='MG')and($dl_nr_code>1)) 
 	{       
+		//echo "<br></br>TEM TUTAJ50";
 		$alc_baza = ZwrocAlc( substr($kod , 8, 11) , $baza, $uzytkownik, $haslo);
 		$alc_baza = UsunSpacje($alc_baza);
 		
 		if ( ( !isset($formalc) || strlen($formalc) == 0 ) && strlen($alc_baza) > 0 && $alc_baza !== "0" )
 		{
+			//echo "<br></br>TEM TUTAJ51";
 			echo include "produkt_gotowy_alc_TEST.php"; 
 			$komunikat = "<center><B><FONT COLOR='#00aa00'>WCZYTAJ KOD ALC <BR>  
 			</center><hr>";
@@ -608,8 +661,10 @@ else
 		}
 		else
 		{
+			//echo "<br></br>TEM TUTAJ52";
 			if( strlen($formalc) > strlen($alc_baza) )
 			{
+				//echo "<br></br>TEM TUTAJ53";
 				$dlugosc_uciecie = strlen($formalc) - strlen($alc_baza);
 				$formalc = substr( $formalc , $dlugosc_uciecie, strlen($alc_baza) );
 			}
@@ -639,6 +694,7 @@ else
 			
 			if( strlen($alc_baza) == 0 || $alc_baza == "0" )
 			{
+				//echo "<br></br>TEM TUTAJ54";
 				//VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
 				
 				require "incl/wys_zapis_b.php";
@@ -650,13 +706,16 @@ else
 			}
 			elseif($alc_baza == $formalc)
 			{
+				//echo "<br></br>TEM TUTAJ55";
 				if ( !isset($licznik) || $licznik == NULL || $licznik <= 0 )
 				{
+					//echo "<br></br>TEM TUTAJ56";
 					$licznik = 1;
 				}
 				
 				if ( $licznik <= ($qty_box - 1) && ( $nazwa_klienta == "KMS" || $nazwa_klienta == "HMMC" || $nazwa_klienta == "KOMOS" ) )
 				{
+					//echo "<br></br>TEM TUTAJ57";
 					$licznik++;
 					echo include "produkt_gotowy_alc_TEST.php"; //inkrementowany $licznik przekazywany do formularza!
 					$komunikat = "<center><B><FONT COLOR='#00aa00'>KOD ALC ( " . ( $licznik - 1 ) . " z " . $qty_box . " ) OK<BR>WCZYTAJ NASTEPNY KOD!<BR>					
@@ -666,6 +725,7 @@ else
 				}
 				else
 				{
+					//echo "<br></br>TEM TUTAJ58";
 					//VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
 				
 					require "incl/wys_zapis_b.php";
@@ -681,6 +741,7 @@ else
 			}
 			else
 			{
+				//echo "<br></br>TEM TUTAJ59";
 				echo include "produkt_gotowy_alc_TEST.php"; 
 				$komunikat = "<center><B><FONT COLOR='#FF0000'>KOD ALC NIEZGODNY !<BR>WCZYTAJ KOD ALC<BR>  
 				</center><hr>";
@@ -693,6 +754,7 @@ else
 	}							
 	else
 	{ 
+		//echo "<br></br>TEM TUTAJ60";
 		echo "<font size='0'>"; 
 		echo include "podaj_part_no_wys.php"; 
 		$komunikat = "<center><B><font size='1'><FONT COLOR='#FF0000'>BOX NIE JEST ZAREJESTROWANY W SYSTEMIE !!! <BR></FONT><FONT COLOR='#0000FF'> PRZYJMIJ BOX DO SYSTEMU !!! 
